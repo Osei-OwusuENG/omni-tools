9b978229cc5d0f52373bb22f7dfa23e6fc2fa029
TheImagoDei <joseiowusu538@gmail.com>
2026-03-13 21:39:20 +0000

    adding pdf data extractor to tools folder

---
+++ thor/tools/pdfdataextractor.py
@@ -0,0 +1,42 @@
+import pdfplumber
+import pandas as pd
+import glob
+import os
+
+
+class extractor():
+    def __init__(self):
+        self.extracted = []
+        pass
+    
+    def extract_tables(self, foldername):
+
+        files = glob.glob(os.path.join('./data', foldername))
+
+        for file in files:
+            with pdfplumber.open(file) as pdf:
+                for page in pdf.pages:
+                    table = page.extract_tables()
+                    if table:
+                        self.extracted.append(table)
+
+        return self.extracted
+    
+
+    def extract_text(self, foldername):
+
+        files = glob.glob(os.path.join('./data', foldername))
+
+        for file in files:
+            with pdfplumber.open(file) as pdf:
+                for page in pdf.pages:
+                    table = page.extract_text()
+                    if table:
+                        self.extracted.append(table)
+
+        return self.extracted
+
+
+
+
+
